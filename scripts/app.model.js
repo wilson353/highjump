4a5f1575de22d946f6681ed0b36a6c42132e3ddb
wilson353 <steve.wilson@aspect.com>
2015-03-30 10:40:57 -0500

    nav refactoring

---
+++ scripts/app.model.js
@@ -0,0 +1,22 @@
+highjump.model = {
+
+    /**
+     * Initialize model
+     * - Load model specific code
+     */
+    init: function() {
+        this.loadpushnav();
+    },
+
+    /**
+     * Load push navigation into DOM
+     * - Loading externally to allow reuse of markup across multiple pages
+     */
+    loadpushnav: function() {
+        $.get("_push-nav.html", function(html) { $("#push").replaceWith(html); });
+    }
+};
+
+$(function () {
+    highjump.model.init();
+});
